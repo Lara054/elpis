ac56f3545e381508810fcb25935e0b72e45adf98
Lara054 <katutosi0504@icloud.com>
2025-05-27 20:32:08 +0900

    class属性をclassNameに修正

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -38,7 +38,7 @@ function HomePage() {
             </section>
 
             {/* <!-- エルピスって何 --> */}
-            <section class="about column" id="about">
+            <section className="about column" id="about">
                 <h2>エルピスとは？</h2>
                 <p>エルピスは、統合失調症、双極性障害、うつ病など、精神的な病を抱えている方のための場所です。「治療を受けてきたけれど、改善が見られなかった」「病名はついているけれど、どう付き合っていいかわからない」そんな方が、少しずつ“自分らしさ”を取り戻していくために、カウンセラーと話しながら、一緒に整えていく場所です。
                     医療機関では「改善が難しい」と言われた症状でも、 実際にここで落ち着きを取り戻した方が何人もいます。
@@ -48,11 +48,11 @@ function HomePage() {
             </section>
 
             {/* <!-- どんな人が話を聞いてくれるの？ --> */}
-            <section class="counselor" id="counselor">
-                <div class="counselor-main container row fade-in">
-                    <div class="counselor-left ">
+            <section className="counselor" id="counselor">
+                <div className="counselor-main container row fade-in">
+                    <div className="counselor-left ">
                         <h2>カウンセラー紹介</h2>
-                        <p class="counselor-name">ウォニョン</p>
+                        <p className="counselor-name">ウォニョン</p>
                         <p>
                             自分を変えたいと思う人の背中を、私は本気で押します。
                             私のやり方は、少し厳しいかもしれません。
@@ -67,22 +67,22 @@ function HomePage() {
                         </p>
                         <Button text="カウンセラーの想い" link="/"></Button>
                     </div>
-                    <div class="counselor-right">
+                    <div className="counselor-right">
                         <img src="../images/person/wonyon.jpg" alt="今西三千代の写真" />
                     </div>
                 </div>
-                <div class="support-member container column fade-in">
+                <div className="support-member container column fade-in">
                     <h3>サポートメンバー</h3>
-                    <div class="support-member-items row">
-                        <div class="support-member-item column">
+                    <div className="support-member-items row">
+                        <div className="support-member-item column">
                             <img src="../images/person/wonyon.jpg" alt="サポートメンバー写真" />
                             <p>サナ</p>
                         </div>
-                        <div class="support-member-item column">
+                        <div className="support-member-item column">
                             <img src="../images/person/wonyon.jpg" alt="サポートメンバー写真" />
                             <p>ソリュン</p>
                         </div>
-                        <div class="support-member-item column">
+                        <div className="support-member-item column">
                             <img src="../images/person/wonyon.jpg" alt="サポートメンバー写真" />
                             <p>ウンチェ</p>
                         </div>
@@ -92,32 +92,32 @@ function HomePage() {
             </section>
 
             {/* <!-- どんな人が来てるの？ --> */}
-            <section class="use-cases fade-in" id="use-cases">
-                <div class="container column">
+            <section className="use-cases fade-in" id="use-cases">
+                <div className="container column">
                     <h2>対象となる症状やご相談例</h2>
-                    <div class="use-cases-items column">
-                        <div class="use-cases-item row">
-                            <div class="left">
+                    <div className="use-cases-items column">
+                        <div className="use-cases-item row">
+                            <div className="left">
                                 <p>うつ状態にある</p>
                             </div>
-                            <div class="center"></div>
-                            <div class="right">
+                            <div className="center"></div>
+                            <div className="right">
                                 <p>子供が言うことを聞いてくれない</p>
                             </div>
                         </div>
-                        <div class="use-cases-item row">
-                            <div class="left"></div>
-                            <div class="center">
+                        <div className="use-cases-item row">
+                            <div className="left"></div>
+                            <div className="center">
                                 <p>統合失調に苦しんでいる</p>
                             </div>
-                            <div class="right"></div>
+                            <div className="right"></div>
                         </div>
-                        <div class="use-cases-item row">
-                            <div class="left">
+                        <div className="use-cases-item row">
+                            <div className="left">
                                 <p>生きるのが辛い</p>
                             </div>
-                            <div class="center"></div>
-                            <div class="right">
+                            <div className="center"></div>
+                            <div className="right">
                                 <p>よる眠ることができない</p>
                             </div>
                         </div>
@@ -125,22 +125,22 @@ function HomePage() {
                 </div>
 
             </section>
-            <section class="access fade-in" id="access">
-                <div class="container column">
+            <section className="access fade-in" id="access">
+                <div className="container column">
                     <h2>アクセス</h2>
                     <img src="../images/map.png" alt="地図" />
-                    <div class="info column">
+                    <div className="info column">
 
-                        <div class="map-info row">
+                        <div className="map-info row">
                             <h3>所在地：</h3>
                             <div>111-1111 千葉県〇〇市〇〇町1-1-1</div>
                         </div>
                         <div>＊最寄の駅（東金駅、成東駅）まで車でお迎えにあがります。</div>
-                        <div class="map-info row">
+                        <div className="map-info row">
                             <h3>電話：</h3>
                             <div>000-0000-0000</div>
                         </div>
-                        <div class="map-info row">
+                        <div className="map-info row">
                             <h3>メール：</h3>
                             <div>test@example.com</div>
                         </div>
