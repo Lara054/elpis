d0a393745b1a5aa66583443a73bfc90d1d793dce
Lara054 <katutosi0504@icloud.com>
2025-06-07 13:44:47 +0900

    Updates

---
+++ precache-manifest.372fe85c0aefaadc86c1b2b74d183702.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9754720f4a05be1fdd7b206cc0dc7c",
+    "url": "/koinonia/index.html"
+  },
+  {
+    "revision": "166b61d415dbc0282f0e",
+    "url": "/koinonia/static/css/main.2023748f.chunk.css"
+  },
+  {
+    "revision": "010402dfde5f99f099ac",
+    "url": "/koinonia/static/js/2.3e972825.chunk.js"
+  },
+  {
+    "revision": "1aef9e0d481a6908c30509eb88500877",
+    "url": "/koinonia/static/js/2.3e972825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75801e565d895daffc5",
+    "url": "/koinonia/static/js/3.e7b39da5.chunk.js"
+  },
+  {
+    "revision": "166b61d415dbc0282f0e",
+    "url": "/koinonia/static/js/main.ac6fd2d7.chunk.js"
+  },
+  {
+    "revision": "55804a95922c8b2f5d64",
+    "url": "/koinonia/static/js/runtime-main.cbe2ae8e.js"
+  }
+]);
