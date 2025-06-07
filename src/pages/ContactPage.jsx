58651d304d2d870ac25aa19488b2b39cfe269552
Lara054 <katutosi0504@icloud.com>
2025-06-07 16:43:17 +0900

    お問い合わせフォーム修正

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -10,7 +10,7 @@ function ContactPage() {
             <Hero title={"お問い合わせ"} />
             <div style={styles.container}>
 
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '10vh' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '10vh', width: '100%' }}>
                     <h2>お問い合わせフォーム</h2>
                     <a
                         href="https://docs.google.com/forms/d/e/1FAIpQLSeiW41pROSq-cvV2KFr_EDVEYtP2ldYzhpwYqPnMZKbKfvhIg/viewform?usp=dialog"
@@ -44,11 +44,15 @@ function ContactPage() {
 const styles = {
     container: {
         maxWidth: '500px',
-        margin: '80px 20px',
+        margin: '80px auto',
         padding: '10px',
         background: '#fff',
         borderRadius: '12px',
         boxShadow: '0 0 10px rgba(0,0,0,0.1)',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 };
 
