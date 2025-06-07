7079e7010e3ed21d964074903154d7e2d0da5999
Lara054 <katutosi0504@icloud.com>
2025-06-07 14:56:04 +0900

    意味わからない画像の削除

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,11 +8,6 @@ function Hero({ title, subTitle }) {
                 {title && <h1 className="hero__title">{title}</h1>}
                 {subTitle && <h3 className="hero__subTitle">{subTitle}</h3>}
             </div>
-            <img src={`${process.env.PUBLIC_URL}/images/hero/hero-background-image.png`} alt="" />
-            <img src={`${process.env.PUBLIC_URL}/images/hero/hero-nav-thinking.png`} alt="" width="100" />
-            <img src={`${process.env.PUBLIC_URL}/images/hero/hero-nav-speaking.png`} alt="" width="100" />
-            <img src={`${process.env.PUBLIC_URL}/images/hero/hero-nav-listening.png`} alt="" width="100" />
-            <img src={`${process.env.PUBLIC_URL}/images/hero/hero-nav-helper.png`} alt="" width="100" />
         </div>
     );
 }
